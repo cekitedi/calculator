58c8e82d139ac2747d44f0289df8b4116a7e290c
cekitedi <edite.cekita@gmail.com>
2018-08-23 20:28:28 +0300

    update calculator_1

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,7 +8,33 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        return "0";
+        System.out.println(expression[0]);
+        System.out.println(expression[1]);
+        System.out.println(expression[2]);
+        double a = Double.parseDouble(expression[0]);
+        //int b = Integer.parseInt(expression[1]);
+        double b = Double.parseDouble(expression[2]);
+
+        double res = 0;
+        switch (expression[1]) {
+            case "+":                res = a + b;                break;
+            case "-":                res = a - b;                break;
+            case "*":                res = a * b;                break;
+            case "/":                res = a / b;                break;
+            //default:                 res = 0;
+        }
+        System.out.println("Length : " + expression.length);
+        if (expression.length>3) {
+            double c = Double.parseDouble(expression[4]);
+            switch (expression[3]) {
+                case "+":                    res = res + c;                    break;
+                case "-":                    res = res - c;                    break;
+                case "*":                    res = res * c;                    break;
+                case "/":                    res = res / c;                    break;
+                // default:                 res = 0;
+            }
+        } // if expression.length
+        return String.valueOf(res);
     }
 
 }
