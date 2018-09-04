1b599565a2998e70ce9ac132336382e3b0cb7a81
cekitedi <edite.cekita@gmail.com>
2018-09-04 18:43:42 +0300

    update calculator_4

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
  * @version 1.0
@@ -7,7 +10,7 @@ package calculator;
  */
 class Calculator {
     String calculate(String[] expression) {
-        System.out.println("Length : " + expression.length);
+        //System.out.println("Length : " + expression.length);
         //System.out.println(expression[0]);
         //System.out.println(expression[1]);
        // System.out.println(expression[2]);
@@ -45,7 +48,8 @@ class Calculator {
                 // default:                 res = 0;
             }
             */
-        //1+3-5-8...
+     /*
+     * //1+3-5-8...
         double res = Double.parseDouble(expression[0]);
         // kazdij vtoroj iz massiva
         for(int i = 1; i<expression.length; i+=2){
@@ -62,8 +66,43 @@ class Calculator {
                 //default:                 res = 0;
             }
             System.out.println("Rezult : " +res);
-      }
+      }*/
+        List<String> tmp = new ArrayList<>();
+        double a = Double.parseDouble(expression[0]);
 
+        for(int i = 1; i<expression.length; i+=2){
+            System.out.println(expression[i]);
+            String opr = expression[i];
+            double b = Double.parseDouble(expression[i+1]);
+            //expression.
+            switch (expression[i]) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(opr);
+                    a=b;
+                    break;
+                case "*":    a*= b;                break;
+                case "/":    a/= b;                break;
+                //default:                 res = 0;
+            }
+            //System.out.println("Rezult : " +res);
+      }
+        tmp.add(String.valueOf(a));
+        System.out.println(tmp);
+        double res =Double.parseDouble(tmp.get(0));
+        for(int i = 1; i<tmp.size(); i+=2){
+            System.out.println(expression[i]);
+            String op = tmp.get(i);
+            double b = Double.parseDouble(tmp.get(i+1));
+            //expression.
+            switch (op) {
+                case "+":                res   += b;                break;
+                case "-":                res   -= b;                break;
+                default:                 res = 0;
+            }
+            //System.out.println("Rezult : " +res);
+        }
        return String.valueOf(res);
     }
 
